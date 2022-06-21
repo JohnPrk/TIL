cc3401f52c98192685f1f26fc28bcf4973928c73
JohnPrk <uo3641493@gmail.com>
2022-06-22 00:51:07 +0900

    6th commit

---
+++ Algorithm/코딩테스트 준비/src/codingTest_programmers_pac/카카오_월간_코드_챌린지_시즌1_3진법_뒤집기.java
@@ -0,0 +1,18 @@
+package codingTest_programmers_pac;
+
+import java.util.*;
+
+public class 카카오_월간_코드_챌린지_시즌1_3진법_뒤집기 {
+	public int solution(int n) {
+        int answer = 0;
+    
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        
+        while() {
+        	
+        }
+        
+        return answer;
+    }
+}
