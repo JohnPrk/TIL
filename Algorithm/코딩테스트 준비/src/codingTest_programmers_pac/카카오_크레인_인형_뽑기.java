16623c591df47076c763db227a565afe81a0405e
JohnPrk <uo3641493@gmail.com>
2022-06-29 23:16:30 +0900

    8th commit

---
+++ Algorithm/코딩테스트 준비/src/codingTest_programmers_pac/카카오_크레인_인형_뽑기.java
@@ -0,0 +1,29 @@
+package codingTest_programmers_pac;
+
+import java.util.*;
+
+public class 카카오_크레인_인형_뽑기 {
+	
+	public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for(int m : moves) {
+            for(int i=0; i < board[m-1].length ; i++) {
+                if(board[i][m-1] != 0) {
+                    if(stack.peek() == board[i][m-1]){
+                        answer += 2;
+                        stack.pop();
+                    }else {
+                        stack.push(board[i][m-1]);
+                    }
+                    board[i][m-1] = 0;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
