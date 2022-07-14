932393fa8046daa5c69136188c19527f4dbb4be7
JohnPrk <uo3641493@gmail.com>
2022-07-14 18:06:09 +0900

    22th commit

--- Algorithm/코딩테스트 준비/src/프로그래머스/행렬_테두리_회전하기.java
+++ Algorithm/코딩테스트 준비/src/프로그래머스/행렬_테두리_회전하기.java
@@ -4,105 +4,127 @@ package 프로그래머스;
 import java.util.*;
 
 
-class Position{
-	
-	int col;
-	int row;
-	
-	Position() {};
-	Position(int col, int row) {
-		this.col = col;
-		this.row = row;
-	}
-	
-	
-}
 public class 행렬_테두리_회전하기 {
 	
-	static int[][] board;
-	static int[][] queries;
-	
-	static int rows;
-	static int cols;
-	static int[] dx = {0, -1, -0, 1};
-	static int[] dy = {1, 0, -1, 0};
-	
-	public void rollin(int left_col, int left_row, int right_col, int right_row) {
+	static int min = 10010;
+
+	public void DFS(int D,int x, int y, int temp_number, int[][] board, int cnt, int c_cnt, int rowus, int colus) {
 		
-		for(int i= left_col; i <= right_col; i++) {
-			for(int j= left_row; j <= right_row; j++) {
-				
-				int cnt=0;
-				int tmp=0;
-					
-					for(int a= 0; a < right_row - left_row; a++) {
-						tmp = board[i+dx[cnt]][j+dy[cnt]];		
-						board[i+dx[cnt]][j+dy[cnt]] = board[i][j];
-					}
-					cnt++;
-					
-					for(int a= 0; a < right_col - left_col; a++) {
-						tmp = board[i+dx[cnt]][j+dy[cnt]];		
-						board[i+dx[cnt]][j+dy[cnt]] = board[i][j];
-					}
-					cnt++;
-					
-					for(int a= 0; a < right_row - left_row; a++) {
-						tmp = board[i+dx[cnt]][j+dy[cnt]];		
-						board[i+dx[cnt]][j+dy[cnt]] = board[i][j];
-					}
-					cnt++;
-					
-					for(int a= 0; a < right_col - left_col; a++) {
-						tmp = board[i+dx[cnt]][j+dy[cnt]];		
-						board[i+dx[cnt]][j+dy[cnt]] = board[i][j];
-					}
-					cnt++;
-					
-					
-					
-//System.out.print(board[i][j] + " ");
-			}
-			System.out.println();
+        int[] dx = {0, 1, -0, -1};
+	    int[] dy = {1, 0, -1, 0};
+		int[] dz = {rowus, colus,rowus,colus};
+		
+		
+		int tmp =0;
+		
+		if(D == ((colus) * 2 + (rowus) * 2)) {
+			return;
 		}
-	}
-	
+		
+		int nx = x + dx[cnt];
+		int ny = y + dy[cnt];
+		
+        
+		tmp = board[nx][ny];
+        
+		if(D == 0) {
+			board[nx][ny] = board[x][y];
+			if(min > board[x][y]) {
+                min = board[x][y];
+		    }
+        }
+		else {
+			board[nx][ny] = temp_number;
+			if(min > temp_number) {
+				min = temp_number;
+			}
+		}
+		
+		
+		c_cnt++;
+		
+		if(c_cnt == dz[cnt]) {
+			cnt++;
+			c_cnt =0;
+		}
+		
+		
+		DFS(D+1, nx, ny, tmp, board, cnt, c_cnt, rowus, colus);
+		
+        
+        cnt =0;
+		nx =0;
+		ny =0;
+        
+	    }
+    
+
+    
 	public int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = {};
+        
+		int[] answer = new int[queries.length];
+        int[][] board = new int[rows+1][columns+1];
+        int cnt=0;
+        int c_cnt =0;
+        int answer_cnt=0;
+        int left_col=0;
+		int left_row =0;
+        int right_col=0;
+        int right_row=0;
+        int colus=0;
+        int rowus=0;
+        
+        
+        int board_cnt=1;
+		for(int i=1; i <= rows; i++) {
+			for(int j=1; j <= columns; j++) {	
+                board[i][j] = board_cnt;	
+                board_cnt++;
+		    }
+		}
         
         for(int[] q : queries) {
-        	int left_col = q[0]-1;
-        	int left_row = q[1]-1;
-        	int right_col = q[2]-1;
-        	int right_row = q[3]-1;
+        	left_col = q[0];
+        	left_row = q[1];
+        	right_col = q[2];
+        	right_row = q[3];
         	
-        	rollin(left_col, left_row, right_col, right_row);
+        	rowus = right_row-left_row;
+        	colus = right_col-left_col;
+        	
+            
+            
+        	DFS(0, left_col, left_row, 0, board, cnt, c_cnt, rowus, colus);
+        	
+            answer[answer_cnt] = min;
+        	answer_cnt++;
+        	
+        	min = 10010;
+
         }
         
+		
         return answer;
     }
-	
-	
+	 
+
 	
 	public static void main(String[] args) {
 		
 		행렬_테두리_회전하기 T = new 행렬_테두리_회전하기();
 		
-		rows = 6;
-		cols = 6;
-		
-		board = new int[cols][rows];
-		queries = new int[][] {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
-		
-		int cnt=1;
-		for(int i=0; i < cols; i++) {
-			for(int j=0; j < rows; j++) {
-				board[i][j] = cnt;
-				cnt++;
-			}
-		}
+		int rows = 6;
+		int cols = 6;
 		
 		
+		
+		int[][] queries = new int [][]{{2,2,5,4}, {3,3,6,6}, {5,1,6,3}};
 		T.solution(rows, cols, queries);
+		
+		
+		
+		
+		
+
 	}
 }
