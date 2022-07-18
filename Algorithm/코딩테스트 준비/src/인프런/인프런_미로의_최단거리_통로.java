e85b48d22af8c5b4024f989d2868a5db345ccb17
JohnPrk <uo3641493@gmail.com>
2022-07-18 11:12:31 +0900

    25th commit

--- Algorithm/코딩테스트 준비/src/인프런/인프런_미로의_최단거리_통로.java
+++ Algorithm/코딩테스트 준비/src/인프런/인프런_미로의_최단거리_통로.java
@@ -2,75 +2,77 @@ package 인프런;
 
 import java.util.*;
 
+class Position {
+	
+	int x, y;
+	
+	Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+}
+
 public class 인프런_미로의_최단거리_통로 {
 
-	static int min = Integer.MAX_VALUE;
+	static int[][] board;
+	static int[][] dis;
 	
-	public void DFS(int x, int y, int[][] board, int[][] dis) {
+	public void BFS(int x, int y) {
 		
+		int[] dx = {0,1,0,-1};
+		int[] dy = {1,0,-1,0};
 		
-		int[] dx = {0, 1, 0, -1};
-		int[] dy = {1, 0, -1, 0};
-		
-		if(board[x][y] == 1) {
-			return;
-		}
-		if(x == 7 && y ==7) {
-			
-			min = Math.min(min, dis[x][y]);
-			return;
-		}
+		Queue<Position> Q = new LinkedList<>();
+		Q.offer(new Position(x, y));	
 		board[x][y] = 1;
 		
-		for(int i=0; i < dx.length; i++) {
+		while(!Q.isEmpty()) {
+			Position tmp = Q.poll();
 			
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			
-			
-			if(nx>=0 && nx<=7 && ny>=0 && ny<=7) {
-				if(board[nx][ny] == 0) {
-					dis[nx][ny] = dis[x][y] +1;
-					DFS(nx, ny, board, dis);
-					board[nx][ny] = 0;
+			for(int i =0; i < dx.length; i++) {
+				
+				int nx = tmp.x + dx[i];
+				int ny = tmp.y + dy[i];
+				
+				
+				if(nx >=1 && nx <=7 && ny >=1 && ny <= 7) {
+					if(board[nx][ny] == 0) {
+						dis[nx][ny] = dis[tmp.x][tmp.y]+1; 
+						Q.offer(new Position(nx, ny));
+						board[nx][ny] = 1;
+					}
 				}
-				
-				
-			
-				
 			}
 		}
-		
+	
+	
 	}
-	
-	
-	public void solution(int[][] board, int[][] dis) {
-		
-		DFS(1,1, board, dis);
-	}
-	
 	
 	public static void main(String[] args) {
 		
-		int[][] board = new int[8][8];
-		int[][] dis = new int[8][8];
 		인프런_미로의_최단거리_통로 T = new 인프런_미로의_최단거리_통로();
 		Scanner scn = new Scanner(System.in);
-	
-	
-		for(int i=1; i <=7; i++) {
-			for(int j=1; j<=7; j++) {
+		
+		board = new int[8][8];
+		dis = new int[8][8];
+		int answer = 0; 
+		
+		for(int i=1; i <8; i++) {
+			for(int j=1; j <8; j++) {
 				board[i][j] = scn.nextInt();
 			}
 		}
 		
 		
-		T.solution(board, dis);
-		if(min ==Integer.MAX_VALUE) {
-			System.out.println(-1);
-		}else {
-			System.out.println(min);
-		}
 		
+		T.BFS(1, 1);
+		
+		if(dis[7][7] == 0) {
+			answer = -1;
+		}else { 
+			answer = dis[7][7];
+		}
+		System.out.println(answer);
 	}
 }
