868bd29ab78789a6eec737bf52ccfcb4bc93caec
JohnPrk <uo3641493@gmail.com>
2022-07-30 20:56:51 +0900

    35th commit

---
+++ Algorithm/코딩테스트 준비/src/스택_큐_프린터.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Print {
+    int  index, importance;
+    
+    Print(int index, int importance) {
+        this.index = index;
+        this.importance = importance;
+    }
+}
+
+class Solution {
+
+    
+    public int solution(int[] priorities, int location) {
+        int turn = 0;
+        
+        ArrayList<Print> printer = new ArrayList<>();
+        
+        for(int i=0; i < priorities.length; i++){
+            printer.add(new Print(i, priorities[i]));
+        }    
+        
+        
+        while(!printer.isEmpty()) {
+            
+            Print print = printer.remove(0);
+            
+            if(printer.stream().anyMatch(otherJob -> print.importance < otherJob.importance)) {
+                printer.add(print);
+            }else {
+                turn++;
+                
+                if(location == print.index) {
+                    break;
+                }
+            }
+            
+        }
+        
+    
+        
+        
+        
+        
+        return turn;
+    }
+}
