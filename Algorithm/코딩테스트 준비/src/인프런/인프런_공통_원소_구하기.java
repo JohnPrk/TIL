92fccc93b6ff9ea47103d6eb67eee3dab2f74da9
JohnPrk <uo3641493@gmail.com>
2022-08-03 15:25:04 +0900

    37th commit

--- Algorithm/코딩테스트 준비/src/인프런/인프런_공통_원소_구하기.java
+++ Algorithm/코딩테스트 준비/src/인프런/인프런_공통_원소_구하기.java
@@ -5,43 +5,50 @@ import java.util.*;
 public class 인프런_공통_원소_구하기 {
 
 	
-	
-	public static void main(String[] args) {
+	public String solution(int[] num1_arr, int[] num2_arr) {
+		String answer = "";
+		int p1 =0;
+		int p2 =0;
 		
-		Scanner scn = new Scanner(System.in);
-		ArrayList<String> list1 = new ArrayList<>();
-		ArrayList<String> list2 = new ArrayList<>();
-		ArrayList<Integer> union = new ArrayList<>();
-		String answer = ""; 	
-		
-		int num = scn.nextInt();
-		
-		for(int i=0; i < num; i++) {
-			list1.add(scn.next());
-		}
-		
-		num = scn.nextInt();
-		
-		for(int i=0; i < num; i++) {
-			list2.add(scn.next());
-		}
-		
-		
-		
-		for(String i : list1) {
-			if(list2.remove(i)) {
-				union.add(Integer.parseInt(i));
+		Arrays.sort(num1_arr);
+		Arrays.sort(num2_arr);
+		while(p1 < num1_arr.length && p2 < num2_arr.length) {
+			if(num1_arr[p1] == num2_arr[p2]) {
+				answer += num1_arr[p1] + " ";
+				p1++;
+				p2++;
+			}else if(num1_arr[p1] > num2_arr[p2]) {
+				p2++;
+			}else if(num1_arr[p1] > num2_arr[p2]) {
+				p1++;
 			}
 		}
+		return answer;
 		
-		Collections.sort(union);
+	}
+	
+	public static void main(String[] args) {
+	
 		
-		for(int u : union) {
-			answer += u + " ";
-			
+		Scanner scn = new Scanner(System.in);
+		인프런_공통_원소_구하기 T = new 인프런_공통_원소_구하기();
+		
+		int num1 = scn.nextInt();
+		int[] num1_arr = new int[num1];
+		
+		for(int i=0; i < num1; i++) {
+			num1_arr[i] = scn.nextInt();
 		}
-		answer = answer.trim();
+		int num2 = scn.nextInt();
+		int[] num2_arr = new int[num2];
 		
+		for(int i=0; i < num2; i++) {
+			num2_arr[i] = scn.nextInt();
+		}
+		
+		
+		String answer = T.solution(num1_arr, num2_arr);
+		answer = answer.strip();
 		System.out.println(answer);
 	}
 }
