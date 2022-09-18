382a6de8e3d1dee34ec7679d8d4a88f9202ae038
JohnPrk <uo3641493@gmail.com>
2022-09-18 17:39:06 +0900

    68th commit

--- Algorithm/코딩테스트 준비/src/백준/브루트포스_실버_14888.java
+++ Algorithm/코딩테스트 준비/src/백준/브루트포스_실버_14888.java
@@ -2,31 +2,33 @@ package 백준;
 import java.util.*;
 
 public class 브루트포스_실버_14888 {
-	static ArrayList<Integer> ans;
-	public static void DFS(String operation) {
+	static HashSet<String[]> operations;
+	public static HashSet<String[]> Permutations(int depth, int end, HashMap<String, Integer> hash, String[] array) {
+		if(depth  == (end-1)) {
+			
+		}
 		
-		
-		
+		return operations;
 	}
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int num = scn.nextInt();
 		String[] op = new String[] {"+", "-", "*", "%"};
-		ArrayList<String> operation = new ArrayList<>();
+		int count = 0;
+		operations = new HashSet<>();
+		HashMap<String, Integer> hash = new HashMap<>();
 		int[] numbers = new int[num];
 		int[] copy = new int[numbers.length];
-		ans = new ArrayList<>();
 		for(int i=0; i < num; i++) numbers[i] = scn.nextInt(); 
 		for(int i=0; i < 4; i++) {
 			int cnt = scn.nextInt();
-			if(cnt>= 1) for(int j =0; j < cnt; j++) operation.add(op[i]);
+			if(cnt>= 1) for(int j =0; j < cnt; j++) {
+				hash.put(op[i], hash.getOrDefault(op[i], 0) + 1);
+				count ++;
+			}
 		}
 		for(int i=0; i < numbers.length; i++) copy[i] = numbers[i];
-		for(int i=0; i < operation.size(); i++) {
-			DFS(operation.get(i));
-			DFS("");
-		}
-//		DFS(0, numbers[0], copy, operation);
+		Permutations(0, count, hash, new String[5]);
 		
 	}
 }
