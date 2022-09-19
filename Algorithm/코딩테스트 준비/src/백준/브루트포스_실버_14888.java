feed14cf2c1b2727d04f1c57c84d2dc68ed7a805
JohnPrk <uo3641493@gmail.com>
2022-09-19 12:35:04 +0900

    69th commit

--- Algorithm/코딩테스트 준비/src/백준/브루트포스_실버_14888.java
+++ Algorithm/코딩테스트 준비/src/백준/브루트포스_실버_14888.java
@@ -3,10 +3,22 @@ import java.util.*;
 
 public class 브루트포스_실버_14888 {
 	static HashSet<String[]> operations;
-	public static HashSet<String[]> Permutations(int depth, int end, HashMap<String, Integer> hash, String[] array) {
+	public static HashSet<String[]> Permutations(int depth, int end, HashMap<String, Integer> hash, String[] array, String[] op) {
 		if(depth  == (end-1)) {
-			
+			operations.add(array);
+		}else {
+			for(int i=0; i < 4; i++) {
+				int cnt = hash.getOrDefault(op[i], 0);
+				for(int j =0; j < cnt; j++) {
+					array[depth] += op[i];
+					hash.put(op[i], hash.get(op[i])-1);
+				}
+			}
 		}
+		
+		
+			
+		
 		
 		return operations;
 	}
@@ -17,18 +29,16 @@ public class 브루트포스_실버_14888 {
 		int count = 0;
 		operations = new HashSet<>();
 		HashMap<String, Integer> hash = new HashMap<>();
+		Stack<String> stack = new Stack<>();
+		Stack<String> copy = new Stack<>();
 		int[] numbers = new int[num];
-		int[] copy = new int[numbers.length];
 		for(int i=0; i < num; i++) numbers[i] = scn.nextInt(); 
 		for(int i=0; i < 4; i++) {
 			int cnt = scn.nextInt();
-			if(cnt>= 1) for(int j =0; j < cnt; j++) {
-				hash.put(op[i], hash.getOrDefault(op[i], 0) + 1);
-				count ++;
-			}
+			if(cnt>= 1) for(int j =0; j < cnt; j++) stack.add(op[j]);
 		}
-		for(int i=0; i < numbers.length; i++) copy[i] = numbers[i];
-		Permutations(0, count, hash, new String[5]);
+		for(int i =0; i < stack.size(); i++) copy.add(stack.pop());
+		Permutations(0, count, hash, new String[count], op);
 		
 	}
 }
