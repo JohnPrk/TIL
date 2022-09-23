a93c88de6b881888093b448b83e058dc72d06533
JohnPrk <uo3641493@gmail.com>
2022-09-23 18:23:13 +0900

    73th commit

--- Algorithm/코딩테스트 준비/src/인프런/인프런_쇠막대기.java
+++ Algorithm/코딩테스트 준비/src/인프런/인프런_쇠막대기.java
@@ -12,10 +12,8 @@ public class 인프런_쇠막대기 {
 		for(int i =1; i < str_array.length; i++) {
 			if(str_array[i].equals(")")) {
 				stack.pop();
-				if(str_array[i-1].equals("(")) {
-					System.out.println(stack);
-					answer += stack.size();
-				}
+				if(str_array[i-1].equals("(")) answer += stack.size();
+				else answer += 1;
 			}else stack.add(str_array[i]);
 		}
 		System.out.println(answer);
