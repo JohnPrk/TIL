446bb4b4501832c1a5d1cbdb5c3fd842991af38f
JohnPrk <uo3641493@gmail.com>
2022-09-27 00:45:29 +0900

    76th commit

--- Algorithm/코딩테스트 준비/src/프로그래머스/DP_정수_삼각형.java
+++ Algorithm/코딩테스트 준비/src/프로그래머스/DP_정수_삼각형.java
@@ -7,29 +7,16 @@ public class DP_정수_삼각형 {
 		int[][] triangle = new int[][] {{7}, {3,8}, {8,1,0}, {2,7,4,4}, {4,5,2,6,5}};
 		int[][] board = new int[triangle.length][triangle.length];
 		int[][] dp = new int[board.length][board[0].length];
-		
-		for(int i=0; i < triangle.length; i++) {
-			for(int j=0; j < triangle[i].length; j++) {
-//				System.out.println(i + " " + j);
-//				System.out.println(triangle[i][j]);
-				board[i][j] = triangle[i][j];
-			}
-		}
+		for(int i=0; i < triangle.length; i++) for(int j=0; j < triangle[i].length; j++) board[i][j] = triangle[i][j];
 		dp[0][0] = board[0][0];
 		for(int i =1;i < board.length; i++) {
 			for(int j =0; j <board[0].length; j++) {
-				if(j == 0) {
-					dp[i][j] = board[i][j] + dp[i-1][j];
-				}else if(j == board[0].length-1) {
-					dp[i][j] = board[i][j] + dp[i-1][j-1];
-				}else {
-					dp[i][j] = Math.max(board[i][j] + dp[i-1][j-1], board[i][j] + dp[i-1][j]);
-				}
+				if(j == 0) dp[i][j] = board[i][j] + dp[i-1][j];	
+				else if(j == board[0].length-1) dp[i][j] = board[i][j] + dp[i-1][j-1];	
+				else dp[i][j] = Math.max(board[i][j] + dp[i-1][j-1], board[i][j] + dp[i-1][j]);
 			}
 		}
-		for(int i=0; i < dp[0].length; i++) {
-			max = Math.max(max, dp[dp.length-1][i]);
-		}
+		for(int i=0; i < dp[0].length; i++) max = Math.max(max, dp[dp.length-1][i]);
 		System.out.println(max);
 	}
 }
---
+++ Algorithm/코딩테스트 준비/src/프로그래머스/순열_줄_서는_방법.java
@@ -0,0 +1,10 @@
+package 프로그래머스;
+import java.util.*;
+public class 순열_줄_서는_방법 {
+	public static void main(String[] args) {
+		int n = 3;
+		int k = 5;
+		int[] numbers = new int[n];
+		for(int i= 1; i <= n; i++) numbers[i-1] = i; 
+	}
+}
