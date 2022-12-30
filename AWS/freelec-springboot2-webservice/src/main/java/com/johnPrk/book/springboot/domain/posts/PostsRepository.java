189c68bab734f713fd7dfacb32c8288fc4f3eb5c
박민욱 <88137420+JohnPrk@users.noreply.github.com>
2022-12-30 22:59:42 +0900

    Delete AWS/freelec-springboot2-webservice directory

--- AWS/freelec-springboot2-webservice/src/main/java/com/johnPrk/book/springboot/domain/posts/PostsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.johnPrk.book.springboot.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
